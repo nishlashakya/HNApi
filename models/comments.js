de5bfcf5d2c684b8a2087adb3969c4ee02fdc278
nishla <nishla@rumsan.com>
2018-02-19 11:24:13 +0545

    comment model

---
+++ models/comments.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+// create schema
+
+const schema = mongoose.Schema({
+  _id           : { type: ObjectId, auto: true},
+  threadId      : { type: ObjectId, ref: 'Threads' },
+  createdDate   : { type: Date, default: Date.now },
+  commentedBy   : { type: ObjectId, ref: 'Users' },
+  comment       : String
+});
+
+module.exports = mongoose.model('Comments', schema);
