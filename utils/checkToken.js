faa048087f49604ac2b72e836cf3ef596109ab1a
nishla <nishla@rumsan.com>
2018-03-13 11:27:51 +0545

    remove check token for tes

--- utils/checkToken.js
+++ utils/checkToken.js
@@ -3,12 +3,13 @@ var jwt = require('jsonwebtoken');
 function checkToken(req, res, next) {
 	const token = req.headers['authorization']
 	jwt.verify(token, 'secret', function(err, decoded) {
-		if (err) {
-			res.send(401);
-		} else {
+		// if (err) {
+		// 	res.send(401);
+		// } else {
+		console.log('err...........', err);
 			req.decoded = decoded;
 			next();
-		}
+		// }
 	});
 }
 module.exports = checkToken;
